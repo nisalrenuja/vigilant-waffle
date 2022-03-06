553233dae869ae60a6beee368aa125282874f977
Nisal Renuja Palliyaguru <42801983+nisalrenuja@users.noreply.github.com>
2022-03-06 16:52:56 +0530

    images displaying added

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 window.saveDataAcrossSessions = true;
 
+let imageElement = getNewImage(); // get the image element
+let nextImageElement = getNewImage(true); // get the image element
+
 webgazer
   .setGazeListener((data, timestamp) => {
     console.log(data, timestamp);
   })
   .begin();
+
+function getNewImage(next = false) {
+  const img = document.createElement("img");
+  img.src = "https://picsum.photos/1000?" + Math.random();
+  if (next) {
+    img.classList.add("next");
+  }
+  document.body.append(img);
+  return img;
+}
